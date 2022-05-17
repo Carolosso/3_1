8520ced9c4545b25e8ff2e087478f329898e22d1
Carolosso <carolosso@outlook.com>
2022-05-17 08:52:06 +0000

    Commit3_0.

--- Main.java
+++ Main.java
@@ -1,6 +1,14 @@
 import java.io.IOException;
 
 class Main {
+  
+  public static void wyczysc(){
+    //wyczyszczenie ekranu
+    System.out.print("\033[H\033[2J");
+    System.out.flush();
+    //-----------------
+  }
+  
   public static void main(String[] args) {
     try {
       Service1 s = new Service1();
