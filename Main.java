5635efd7c98afb822748310c25811d169f066cd5
Carolosso <carolosso@outlook.com>
2022-05-17 08:57:11 +0000

    Commit3_1.

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 import java.io.IOException;
 
 class Main {
@@ -12,13 +13,43 @@ class Main {
   public static void main(String[] args) {
     try {
       Service1 s = new Service1();
-      s.addStudent(new Student("Krzysztof", 20));
-      s.addStudent(new Student("Janusz", 40));
-
-      var students = s.getStudents();
-      for(Student current : students) {
-        System.out.println(current.ToString());
+      Scanner scan = new Scanner(System.in);
+      int wybor=0;
+      while(wybor!=9){
+      System.out.println("-----------MENU------------");
+      System.out.println("1. Dopisz studenta.");
+      System.out.println("2. Pokaz studentow.");
+      System.out.println("9. Wyjscie.");
+      wybor=scan.nextInt();
+      scan.nextLine();
+          switch(wybor){
+            case 1: {
+              wyczysc();
+              System.out.println("Wybrano opcje 1. ");
+              System.out.println("Podaj imie: ");
+              String imie=scan.nextLine();
+              System.out.println("Podaj wiek: ");
+              int wiek=scan.nextInt(); scan.nextLine();
+              s.addStudent(new Student(imie, wiek));
+              wyczysc();
+              break;
+            }
+            case 2: {
+              wyczysc();
+              System.out.println("Wybrano opcje 2. ");
+              //------------wypisywanie
+              var studenci = s.getStudents();
+              for(Student aktaulny : studenci) {
+                System.out.println(aktaulny.ToString());
+              }
+              //------------------
+              scan.nextLine();//by rozwiazac problem z pomijaniem scannera po nextInt
+              wyczysc();
+              break;
+            }  
+          }
       }
+        scan.close();
     } catch (IOException e) {
 
     }
